8d9f486320caca96d431eefddeb45470fda8463d
Rahul Rai <rahulrairr2014@gmail.com>
2022-10-14 07:16:02 +0530

    first commit for contacts app

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import ContactList from "./ContactList";
+import AddContact from "./AddContact";
+
+function App() {
+  return (
+    <div>
+      Hello World
+      <Header />
+      <ContactList />
+      <AddContact />
+    </div>
+  );
+}
+
+export default App;
